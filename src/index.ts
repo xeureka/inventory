79b42439464c5e970b1c0a683ca36057d460cb46
xeureka <teklemariameureka@gmail.com>
2025-12-20 11:54:37 +0300

    db and halth check setup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
+import express from "express"
+import cors from "cors"
+import connectdb from "./config/db"
 
-console.log("hello world")
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+// routes
+app.get("/health",(req:express.Request,res:express.Response) => {
+    try {
+        return res.status(201).json({message: "Every things works fine here"})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json('Server not working')
+    }
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, async ()=> {
+    await connectdb();
+    console.log(`server running on port ${PORT}`)
+})
---
+++ src/req.ts
@@ -0,0 +1,16 @@
+
+// Product (name,description,number of item,added date,category reference)
+// Category (name,description)
+
+
+//End Points for category
+
+// create category
+// delete category
+// update category
+
+
+// End Points for produts
+// create product
+// update category
+// delete cateogry
