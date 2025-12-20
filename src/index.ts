7fa8cbf8e7687cb2783d39dd74c4c051a804f2aa
xeureka <teklemariameureka@gmail.com>
2025-12-20 11:07:45 +0300

    restarting again

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,2 @@
-import express from "express"
-import cors from "cors"
-import connectdb from "./config/db"
-import categoryRoute from "./routes/category.route"
-import productRoute from "./routes/product.route"
 
-const app = express()
-
-app.use(express.json())
-app.use(cors())
-
-app.use("/category",categoryRoute)
-app.use("/product",productRoute)
-
-await connectdb()
-
-app.listen(3000,() => {
-  console.log(`server running on port 3000`)
-})
-
+console.log("hello world")
--- src/routes/category.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import {seeExistingCategories, addNewCategory } from "../controllers/category.controller"
-
-const router = express.Router()
-
-router.get("/list",seeExistingCategories)
-router.post("/new",addNewCategory)
-
-export default router;
