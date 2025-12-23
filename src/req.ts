9d31e36506905340a497044e449270078cffb8ad
xeureka <teklemariameureka@gmail.com>
2025-12-23 11:37:31 +0300

    req fix

--- src/req.ts
+++ src/req.ts
@@ -3,6 +3,3 @@
  * 1. end point for seelling and refilling of items
  */
 
-// what are the things that are no compatible with my workign righer here man
-// how to work with those things configure them and get done with what the offer man
-// 
